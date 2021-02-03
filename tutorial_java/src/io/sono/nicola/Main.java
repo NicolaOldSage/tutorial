ba77b6865b2ad0586616d23d1c4022740a731394
nicola <ndauria9@gmail.com>
2021-02-03 15:16:37 +0100

    progetto java con errore

---
+++ tutorial_java/src/io/sono/nicola/Main.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+
+
+package io.sono.nicola;
+
+import java.util.Scanner;
+
+/**
+ * @author Nicola
+ *
+ */
+public class Main {
+
+	Scanner scan;
+	scan=new Scanner();
+	
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
