d740d2b7b7992fc5b6061f578c6aaa432dc78030
nicola <ndauria9@gmail.com>
2021-02-03 16:06:37 +0100

    ho levato lo scanner

--- tutorial_java/src/io/sono/nicola/Main.java
+++ tutorial_java/src/io/sono/nicola/Main.java
@@ -10,18 +10,17 @@ import java.util.Scanner;
 
 public class Main {
 
-	Scanner scan;
-	scan=new Scanner();
+	
 	
 	public Main() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
 	}
 
